4def8a82fa34ff29eef6c9e2abec5fb567a86936
Ademir Ferreira <ademir.ae86@gmail.com>
2020-02-20 21:55:20 -0300

    Add populate UI with saved data

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save Selected movie index and price
@@ -27,12 +29,28 @@ function updateSelectedCount() {
 
 }
 
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
 
   setMovieData(e.target.selectedIndex, e.target.value);
-  console.log(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 })
 
@@ -44,5 +62,8 @@ container.addEventListener('click', (e) => {
 
     updateSelectedCount();
   }
-})
+});
+
+// Initial count and total set
+updateSelectedCount();
 
